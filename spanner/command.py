de0975220775a8665421a0cf3f59d1da190e6c43
Brett Smith <bc.smith@sas.com>
2014-11-11 17:00:34 +0000

    Straighten out command line

--- spanner/command.py
+++ spanner/command.py
@@ -20,7 +20,7 @@ import sys
 
 from conary.lib import command, options
 
-from spanner import builder
+from spanner import worker
 
 
 logger = logging.getLogger(__name__)
@@ -77,58 +77,42 @@ class ConfigCommand(SpannerCommand):
 
 class BuilderCommand(SpannerCommand):
     commands = ['build']
-    paramHelp = '[plans]...'
+    paramHelp = '[uri] [plans]...'
     help = "Build git repos from bob-plans"
     requireConfig = True
 
     def addParameters(self, argDef):
         SpannerCommand.addParameters(self, argDef)
-        argDef['branch'] = options.MULT_PARAM
-        argDef['cachedir'] = options.ONE_PARAM
-        argDef['repo'] = options.ONE_PARAM
+        argDef['branch'] = options.ONE_PARAM
+        argDef['cfgfile'] = options.ONE_PARAM
         argDef['dry-run'] = options.NO_PARAM
-        argDef['group'] = options.ONE_PARAM
-        argDef['group-label'] = options.ONE_PARAM
-        argDef['external'] = options.NO_PARAM
-        argDef['force-build'] = options.MULT_PARAM
-        argDef['xml'] = options.NO_PARAM
-        argDef['json'] = options.NO_PARAM
+        argDef['group'] = options.NO_PARAM
 
     def shouldRun(self):
-        if self.branches and self.repo:
+        if self.uri:
             return True
-        logger.error('build command requires --repo '
-                     'and at least one --branch <branch>')
+        logger.error('build command requires a uri')
         return False
 
     def runCommand(self, cfg, argSet, params, **kw):
         self.cfg = cfg
-        self.branches = argSet.pop('branch', [])
-        self.cachedir = argSet.pop('cachedir', 'spanner-cache')
-        self.repo = argSet.pop('repo', None)
+        self.cfgfile = argSet.pop('cfgfile', None)
+        self.branches = argSet.pop('branch', None)
         self.test = argSet.pop('dry-run', False)
         self.group = argSet.pop('group', False)
-        self.group_label = argSet.pop('group-label', False)
-        self.external = argSet.pop('external', False)
-        self.targets = argSet.pop('force-build', [])
-        self.xml = argSet.pop('xml', False)
-        self.json = argSet.pop('json', False)
 
-        plans = params[2:]
+        self.uri = params[2]
+        force_builds = params[3:]
 
         if not self.shouldRun():
             logger.info('Builder will not run, exiting.')
             sys.exit(2)
 
-        for branch in self.branches:
-            spanner = builder.Builder(self.repo, branch,
-                                      self.cachedir, self.cfg,
-                                      self.test, plans)
-            if self.targets:
-                spanner.setForceBuild(self.targets)
-            packages, built_packages, failed_packages = spanner.build()
-            if built_packages and self.group:
-                spanner.buildGroup(packages, self.group, self.external)
-            # we only consider it an error if no packages were built
-            # and any failed to build
-            return (1 if not packages and failed_packages else 0)
+        spanner = worker.Worker(    uri=self.uri, 
+                                    force=force_builds, 
+                                    branch=self.branch, 
+                                    cfgfile=self.cfgfile, 
+                                    test=self.test,
+                                    )
+        spanner.main()
+
